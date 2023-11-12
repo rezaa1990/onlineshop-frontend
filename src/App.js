69299d1125fbb37ca1fe514a45f9022b62215e40
Reza <rezaaaa.z.1991@gmail.com>
2023-11-12 13:42:55 +0330

    addminpanel updated to add product

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import AdminPanel from "./components/adminpanel";
 import Banner from "./components/banner";
-import Blog from "./components/blog";
+import Blog from "./components/products";
 import Contact from "./components/contact";
 import Features from "./components/features";
 import Footer from "./components/footer";
@@ -11,6 +11,7 @@ import css from "./css/main.css";
 import Register from "./components/register";
 import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 import Login from "./components/login";
+import UserPanel from "./components/userpanel";
 
 function App() {
   const [admin, setAdmin] = useState(true);
@@ -19,22 +20,23 @@ function App() {
     <BrowserRouter>
     <div className="">
       <Navbar></Navbar>
-      {admin === false ? (
+      {admin === true ? (
         <AdminPanel></AdminPanel>
       ) : (
         <>
-          <Banner></Banner>
+          {/* <Banner></Banner> */}
           
-          <Routes>
+          {/* <Routes>
+          <Route path="/userdashboard" element={<UserPanel/>} />
           <Route path="/login" element={<Login/>} />
           <Route path="/register" element={<Register/>} />
-          </Routes>
+          </Routes> */}
           
-          <Features></Features>
+          {/* <Features></Features> */}
+          {/* <Products></Products> */}
           <Products></Products>
-          <Blog></Blog>
-          <Contact></Contact>
-          <Footer></Footer>
+          {/* <Contact></Contact> */}
+          {/* <Footer></Footer> */}
         </>
       )}
     </div>
--- src/components/adminpanel.jsx
+++ src/components/adminpanel.jsx
@@ -1,17 +1,45 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faCoffee, faDove, faProcedures, faUser, faWarehouse } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import { useState } from 'react';
+import axios from 'axios';
 
 library.add(faCoffee);
 
 function AdminPanel() {
-  const [showHidden, setShowHidden] = useState(false);
+  const [showHidden, setShowHidden] = useState();
+
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [numberOfLikes, setNumberOfLikes] = useState("");
+  const [description, setDescription] = useState("");
+  const [responseMessage, setResponseMessage] = useState("");
+
+  async function handleClick(e) {
+    e.preventDefault();
+    try {
+      const addProduct = {
+        name,
+        price,
+        numberOfLikes,
+        description,
+      };
+  
+      console.log(addProduct);
+  
+      const response = await axios.post(`http://localhost:5000/api/products/addproduct`,addProduct);
+      console.log(response.data.message);
+      console.log(response.data);
+      setResponseMessage(response.data.message)
+      setShowHidden(4)
+    } catch (error) {
+      console.error('خطا در ارسال درخواست:', error);
+    }
+  }
 
   return (
     <>
-      <div className="d-flex">
+      <div className="d-flex p-2">
         <ul className="row nav col-sm-3 h-50 ">
           <li className="nav-item m-auto p-2 shadow-sm">
             <a className="nav-link text-black" href="#" onClick={() => setShowHidden(1)}>
@@ -72,6 +100,47 @@ function AdminPanel() {
           </div>
         </div>
 
+
+        <div className="container-fluid col-sm-10 px-5" style={{ display: showHidden ==3 ? 'block' : 'none' }}>
+
+          <div className="form-group mx-5 my-3">
+          <label htmlFor="email">نام</label>
+          <input onChange={(e)=>setName(e.target.value)} id="email" type="text" className="form-control" />
+        </div>
+        <div className="form-group mx-5 my-3">
+          <label htmlFor="email">قیمت</label>
+          <input onChange={(e)=>setPrice(e.target.value)} id="email" type="text" className="form-control" />
+        </div>
+        <div className="form-group mx-5 my-3">
+          <label htmlFor="email">لایک</label>
+          <input onChange={(e)=>setNumberOfLikes(e.target.value)} id="email" type="text" className="form-control" />
+        </div>
+        <div className="form-group mx-5 my-3">
+          <label htmlFor="email">مشخصات</label>
+          <textarea className="form-control" onChange={(e)=>setDescription(e.target.value)}  name="" id="message" cols="30" rows="3"></textarea>
+        </div>
+        <div className="mx-5">
+            <button className="btn btn-success w-100" onClick={handleClick}>افزودن محصول</button>
+          </div>
+        </div>
+
+        <div className="container-fluid col-sm-11 p-4" style={{ display:'block'}}>
+            <div className="p-2">
+            <p className="text-success">{responseMessage}</p>
+          </div>
+          <div className="p-2 shadow-sm">
+            <p className="">نام:</p>
+            <p className="">{name}</p>
+          </div>
+          <div className="p-2 shadow-sm">
+            <p className="">قیمت</p>
+            <p className="">{price}</p>
+          </div>
+          <div className="p-2 shadow-sm">
+            <p className="">توضیحات</p>
+            <p className="">{description}</p>
+          </div>
+        </div>
 
       </div>
     </>
