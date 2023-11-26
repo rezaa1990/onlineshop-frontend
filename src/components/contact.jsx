f8ea606792bd3447150f7c9a40b28dea13ed97e5
Reza <rezaaaa.z.1991@gmail.com>
2023-11-26 15:51:05 +0330

    sending message  in contact component activated

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,10 +1,37 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faCoffee} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import axios from 'axios';
+import { useState } from 'react';
 
 
-library.add(faCoffee);
 function Contact() {
+  const [senderName,setSenderName]=useState('');
+  const [senderEmail,setSenderEmail]=useState('');
+  const [content,setContent]=useState('');
+  const [sendMessageResponse,setSendMessageResponse]=useState('')
+
+  async function sendMessage(e) {
+    e.preventDefault();
+    try {
+      const addMessage = {
+        senderName,
+        senderEmail,
+        content,
+
+      };
+      console.log(addMessage);
+      const response = await axios.post(`http://localhost:5000/api/message/addmessage`,addMessage);
+      console.log(response.data);
+      setSendMessageResponse(response.data.message);
+      setSenderName('')
+      setSenderEmail('')
+      setContent('')
+    } catch (error) {
+      console.error('خطا:', error);
+    }
+  }
+
   return (
     <section className='p-5'>
       <div className="container-fluid">
@@ -16,22 +43,25 @@ function Contact() {
               <h3 className="text-secondary my-5">آیا سوالی دارید؟</h3>
               <form action="" className="text-muted">
 
+              <div className="form-group">
+                  <p className="text-center text-success">{sendMessageResponse}</p>
+                </div>
 
                 <div className="form-group">
                   <label htmlFor="name">نام</label>
-                  <input id="name" type="text" className="form-control" />
+                  <input value={senderName} onChange={(e)=>setSenderName(e.target.value)} id="name" type="text" className="form-control" />
                 </div>
 
                 <div className="form-group">
                   <label htmlFor="email">ایمیل</label>
-                  <input id="email" type="text" className="form-control" />
+                  <input value={senderEmail} onChange={(e)=>setSenderEmail(e.target.value)} id="email" type="text" className="form-control" />
                 </div>
 
                 <div className="form-group">
                   <label htmlFor="message">پیام</label>
-                  <textarea className="form-control" name="" id="message" cols="30" rows="3"></textarea>
+                  <textarea value={content} onChange={(e)=>setContent(e.target.value)} className="form-control" name="" id="message" cols="30" rows="3"></textarea>
                 </div>
-                <button className="btn btn-outline-success my-1 w-100">ارسال</button>
+                <button className="btn btn-outline-success my-1 w-100" onClick={(e)=>sendMessage(e)}>ارسال</button>
 
               </form>
             </div>
