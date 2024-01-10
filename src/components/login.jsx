cbbe48408d1090572cb7dd8ec4b468e627ddb146
Reza <rezaaaa.z.1991@gmail.com>
2024-01-10 14:23:21 +0330

    login component updated to recieve email and reset password

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -19,26 +19,15 @@ function Login() {
   }=useContext(AppContext);
   
   const  navigate = useNavigate();
-  const[email , setEmail ]= useState("");
-  const[password , setPassword ]= useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [RepeatNewPassword, setRepeatNewPassword] = useState("");
   const [loginResponseMessage, setLoginResponseMessage] = useState();
-  const [display,setDisplay] = useState(1);
-  
-  
-  // const firebaseConfig = {
-    
-  //   apiKey: "AIzaSyBZMUlJZKzLF3sYE8L2OWQmf8YgbwZjk2c",
-  //   authDomain: "rezshop.firebaseapp.com",
-  //   projectId: "rezshop",
-  // };
-  
-  // // بررسی اینکه آیا یک نمونه از Firebase App با نام '[DEFAULT]' ایجاد شده یا خیر
-  // if (!firebase.apps.length) {
-  //   firebase.initializeApp(firebaseConfig); // ایجاد یک نمونه از Firebase App
-  // } else {
-  //   firebase.app(); // استفاده از نمونه ایجاد شده اگر وجود دارد
-  // }
-  
+  const [resetCode, setResetCode] = useState();
+  const [display, setDisplay] = useState(1);
+  const [buttonState, setButtonState] = useState(false);
+
   async function login(e) {
     e.preventDefault();
     try {
@@ -46,91 +35,237 @@ function Login() {
         email,
         password,
       };
-  
+
       console.log(loginData);
-  
-      const response = await axios.post(`http://${server}:5000/api/auth/login`, loginData);
+
+      const response = await axios.post(
+        `http://${server}:5000/api/auth/login`,
+        loginData
+      );
       console.log(response.data);
-      localStorage.setItem('userToken', response.data.data.token);
+      localStorage.setItem("userToken", response.data.data.token);
       handleLogin();
       const role = response.data.data.role;
       navigate(role === "adminUser" ? "/adminpanel" : "/");
     } catch (error) {
-      console.log(error)
+      console.log(error);
       setLoginResponseMessage(error.response?.data?.message);
     }
   }
 
-  async function resetPassword(e) {
+  async function resetPasswordEmail(e) {
+    setButtonState(true);
     e.preventDefault();
     try {
       const loginData = {
         email,
       };
       console.log(loginData);
-      const response = await axios.put(`http://${server}:5000/api/auth/resetpassword`, loginData);
+      const response = await axios.put(
+        `http://${server}:5000/api/auth/resetpasswordemail`,
+        loginData
+      );
       console.log(response);
+      setButtonState(false);
+      setDisplay(3);
     } catch (error) {
-      console.log(error)
-      setLoginResponseMessage(error.response?.data?.message);
+      console.log(error);
+      setLoginResponseMessage(error.response?.data?.data);
+      setButtonState(false);
+    }
+  }
+
+  async function resetPassword(
+    newPassword,
+    RepeatNewPassword,
+    email,
+    resetCode,
+    e
+  ) {
+    setButtonState(true);
+    e.preventDefault();
+    try {
+      const data = {
+        newPassword,
+        RepeatNewPassword,
+        email,
+        resetCode,
+      };
+      console.log(data);
+      const response = await axios.put(
+        `http://${server}:5000/api/auth/resetpasswor`,
+        data
+      );
+      console.log(response);
+      setLoginResponseMessage(response.data?.message);
+      setButtonState(false);
+      setDisplay(1);
+    } catch (error) {
+      console.log(error);
+      console.log(error.response?.data?.data)
+      setLoginResponseMessage(error.response?.data?.data);
+      setButtonState(false);
     }
   }
 
   return (
     <div className="row containe justify-content-center login mx-0">
-    <div className="col-lg-6" style={{display:display === 1 ? "block" : "none"}}>
-      <h3 className="m-4 text-center text-light">ورود</h3>
-
-      <div className="form-group mx-5">
-       <p className="text-center text-danger ">{loginResponseMessage}</p>
-      </div>
-
-      <form action="" className="text-muted mb-5">
-        <div className="form-group mx-5 text-light">
-          <label htmlFor="">ایمیل</label>
-          <input onChange={(e)=>setEmail(e.target.value)} id="login-input" type="text" className="form-control border-0 text-light" />
-        </div>
-
-        <div className="form-group mx-5 text-light">
-          <label htmlFor="">رمز عبور</label>
-          <input onChange={(e)=>setPassword(e.target.value)} id="login-input" type="password" className="form-control border-0 text-light" />
-        </div>
+      {/* login */}
+      <div
+        className="col-lg-6"
+        style={{ display: display === 1 ? "block" : "none" }}
+      >
+        <h3 className="m-4 text-center text-light">ورود</h3>
 
         <div className="form-group mx-5">
-          <button onClick={login} className="btn btn-success my-3 w-100">ورود</button>
+          <p className="text-center text-danger ">{loginResponseMessage}</p>
         </div>
 
-        {/* <div className="form-group mx-5">
-          <button onClick={handleGoogleLogin} className="btn btn-outline-warning my-1 w-100">google</button>
-        </div> */}
+        <form action="" className="text-muted mb-5">
+          <div className="form-group mx-5 text-light">
+            <label htmlFor="">ایمیل</label>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              id="login-input"
+              type="text"
+              className="form-control border-0 text-light"
+            />
+          </div>
 
-        <div className="text-center">
-            <Link to="/register" className="text-light mx-2">ثبت نام</Link>
-            <p onClick={()=>setDisplay(2)} className="text-light mx-2">فراموشی رمز عبور</p>
-        </div>
-      </form>
+          <div className="form-group mx-5 text-light">
+            <label htmlFor="">رمز عبور</label>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              id="login-input"
+              type="password"
+              className="form-control border-0 text-light"
+            />
+          </div>
 
+          <div className="form-group mx-5">
+            <button onClick={login} className="btn btn-success my-3 w-100">
+              ورود
+            </button>
+          </div>
 
-    </div>
-      
-    <div className="d-flex justify-content-center" style={{display:display === 2 ? "block" : "none"}}>
-    <div className="col-lg-6">
-      <h3 className="m-4 text-center text-light">بازیابی رمز عبور</h3>
-      <div className="form-group mx-5">
-       <p className="text-center text-danger ">{loginResponseMessage}</p>
+          <div className="text-center">
+            <Link to="/register" className="text-light mx-2">
+              ثبت نام
+            </Link>
+            <Link onClick={() => setDisplay(2)} className="text-light mx-2">
+              فراموشی رمز عبور
+            </Link>
+          </div>
+        </form>
       </div>
-      <form action="" className="text-muted mb-5">
-        <div className="form-group mx-5 text-light">
-          <label htmlFor="">ایمیل</label>
-          <input onChange={(e)=>setEmail(e.target.value)} id="login-input" type="text" className="form-control border-0 text-light" />
+      {/* send email */}
+      <div
+        className="col-lg-6"
+        style={{ display: display === 2 ? "block" : "none" }}
+      >
+        <div className="text-center">
+          <h3 className="my-4 text-light">
+            {" "}
+            ارسال ایمیل بازیابی رمز عبور
+          </h3>
+          <div className="form-group mx-5">
+            <p className="text-center text-danger ">{loginResponseMessage}</p>
+          </div>
+          <form action="" className="text-muted mb-5">
+            <div className="form-group mx-5 text-light">
+              <label htmlFor="">ایمیل</label>
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                id="login-input"
+                type="text"
+                className="form-control border-0 text-light"
+              />
+            </div>
+            <div className="form-group mx-5 text-center">
+              <button
+                onClick={resetPasswordEmail}
+                disabled={buttonState}
+                className="btn btn-success my-3 w-50"
+              >
+                بازیابی
+              </button>
+            </div>
+          </form>
         </div>
-        <div className="form-group mx-5 text-center">
-          <button onClick={resetPassword} className="btn btn-success my-3 w-50">بازیابی</button>
+      </div>
+      {/* reset password */}
+      <div
+        className="col-lg-6"
+        style={{ display: display === 3 ? "block" : "none" }}
+      >
+        <div className="">
+          <h3 className="m-4 text-center text-light">تعیین رمز عبور جدید</h3>
+          <div className="form-group mx-5">
+            <p className="text-center text-danger ">{loginResponseMessage}</p>
+          </div>
+          <form action="" className="text-muted mb-5">
+            <div className="form-group mx-5 text-light">
+              <label htmlFor="">ایمیل</label>
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                id="login-input"
+                type="text"
+                className="form-control border-0 text-light"
+              />
+            </div>
+            <div className="form-group mx-5 text-light">
+              <label htmlFor="">کد بازیابی</label>
+              <input
+                onChange={(e) => setResetCode(e.target.value)}
+                value={resetCode}
+                id="login-input"
+                type="text"
+                className="form-control border-0 text-light"
+              />
+            </div>
+            <div className="form-group mx-5 text-light">
+              <label htmlFor="">رمز جدید</label>
+              <input
+                onChange={(e) => setNewPassword(e.target.value)}
+                value={newPassword}
+                type="password"
+                id="login-input"
+                className="form-control border-0 text-light"
+              />
+            </div>
+            <div className="form-group mx-5 text-light">
+              <label htmlFor="">تکرار رمز جدید</label>
+              <input
+                onChange={(e) => setRepeatNewPassword(e.target.value)}
+                value={RepeatNewPassword}
+                type="password"
+                id="login-input"
+                className="form-control border-0 text-light"
+              />
+            </div>
+            <div className="form-group mx-5 text-center">
+              <button
+                onClick={(e) =>
+                  resetPassword(
+                    newPassword,
+                    RepeatNewPassword,
+                    email,
+                    resetCode,
+                    e
+                  )
+                }
+                disabled={buttonState}
+                className="btn btn-success my-3 w-50"
+              >
+                تغییر رمز
+              </button>
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
     </div>
-    </div>
-  </div>
   );
 }
 
