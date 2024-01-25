71b22e90b5e05046cf58c3bbd697a9d3121782ff
Reza <rezaaaa.z.1991@gmail.com>
2024-01-25 16:09:35 +0330

    navbar updated and collapse button bug fixed

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -221,19 +221,20 @@ function Navbar() {
 
   return (
     <nav
-      className={`navbar navbar-expand-lg container-fluid position-fixed z-1 ${scrolled ? 'py-0' : 'py-3'}`}
+      className={`navbar navbar-expand-lg container-fluid position-fixed z-1 ${
+        scrolled ? "py-0" : "py-3"
+      }`}
       style={{
-        opacity: '.9',
-        transition: 'padding 0.3s ease-in-out', // افزودن انیمیشن به ترانزیشن
+        opacity: ".9",
+        transition: "padding 0.3s ease-in-out", // افزودن انیمیشن به ترانزیشن
       }}
     >
       <div className="container-fluid">
-
         <div
           className="navbar-toggler px-1 bg-light"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
+          // data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent"
           aria-expanded="false"
           aria-label="Toggle navigation"
@@ -242,86 +243,170 @@ function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </div>
 
-        <div className={`collapse navbar-collapse ${isCollapsed ? '' : 'show'}`} id="navbarSupportedContent">
-         
+        <div
+          className={`collapse navbar-collapse ${isCollapsed ? "" : "show"}`}
+          id="navbarSupportedContent"
+        >
           <ul className="navbar-nav p-0">
-
             <li className="nav-item rounded">
-              <Link to="./userdashboard" className="nav-link text-white rounded" style={{backgroundColor: active == 1 ? "rgb(37, 0, 122)" : ''}} onClick={()=>handleActive(1)}>
-                <i className="" style={{ fontSize: '15px', cursor: 'pointer' }}>
-                  <img src={userIcon} className="text-dark" style={{ cursor: 'pointer', width: '20px', height: '20px' }} />
-                </i>حساب کاربری
+              <Link
+                to="./userdashboard"
+                className="nav-link text-white rounded"
+                style={{
+                  backgroundColor: active == 1 ? "rgb(37, 0, 122)" : "",
+                }}
+                onClick={() => handleActive(1)}
+              >
+                <i className="" style={{ fontSize: "15px", cursor: "pointer" }}>
+                  <img
+                    src={userIcon}
+                    className="text-dark me-2"
+                    style={{ cursor: "pointer", width: "20px", height: "20px" }}
+                  />
+                </i>
+                حساب کاربری
               </Link>
             </li>
 
             <li className="nav-item rounded">
-              <Link to="/" className="nav-link text-white rounded" style={{backgroundColor: active == 2 ? "rgb(37, 0, 122)" : ''}} onClick={()=>handleActive(2)}>
-                <i className="" style={{ fontSize: '15px', cursor: 'pointer' }}>
-                  <img src={home} className="mx-1" style={{ cursor: 'pointer', width: '20px', height: '20px' }} />
+              <Link
+                to="/"
+                className="nav-link text-white rounded"
+                style={{
+                  backgroundColor: active == 2 ? "rgb(37, 0, 122)" : "",
+                }}
+                onClick={() => handleActive(2)}
+              >
+                <i className="" style={{ fontSize: "15px", cursor: "pointer" }}>
+                  <img
+                    src={home}
+                    className="me-2"
+                    style={{ cursor: "pointer", width: "20px", height: "20px" }}
+                  />
                 </i>
                 خانه
               </Link>
             </li>
 
-            <li className="nav-item mx-2 d-flex align-items-start rounded" style={{backgroundColor: active == 3 ? "rgb(37, 0, 122)" : ''}} onClick={()=>handleActive(3)}>
-              <div className="text-white text-center px-2" 
-                  style={{ 
+            <li
+              className="nav-item mx-2 d-flex align-items-start rounded"
+              style={{ backgroundColor: active == 3 ? "rgb(37, 0, 122)" : "" }}
+              onClick={() => handleActive(3)}
+            >
+              <div
+                className="text-white text-center px-2 d-xs-none d-none d-lg-block"
+                style={{
                   backgroundColor: "red",
                   borderTopLeftRadius: "40%",
                   borderTopRightRadius: "40%",
-                  borderBottomRightRadius:"40%",
-                  display:"block"
-                  }}>
-                  {user?.basket?.length || 0}
+                  borderBottomRightRadius: "40%",
+                  display: "block",
+                }}
+              >
+                {user?.basket?.length || 0}
               </div>
 
-              <Link to="./userdashboard" className="nav-link text-white rounded">                
-                <i className="" style={{ fontSize: '15px', cursor: 'pointer' }}>
-                <img src={basket} className="" style={{ cursor: 'pointer', width: '25px', height: '25px' }} />
-                سبد خرید
+              <Link
+                to="./userdashboard"
+                className="nav-link text-white rounded"
+              >
+                <i className="" style={{ fontSize: "15px", cursor: "pointer" }}>
+                  <img
+                    src={basket}
+                    className=""
+                    style={{ cursor: "pointer", width: "25px", height: "25px" }}
+                  />
+                  سبد خرید
                 </i>
               </Link>
+              <div
+                className="text-white text-center px-2 d-lg-none"
+                style={{
+                  backgroundColor: "red",
+                  borderTopLeftRadius: "40%",
+                  borderTopRightRadius: "40%",
+                  borderBottomRightRadius: "40%",
+                  display: "block",
+                }}
+              >
+                {user?.basket?.length || 0}
+              </div>
             </li>
 
-            <Dropdown className='rounded' style={{backgroundColor: active == 4 ? "rgb(37, 0, 122)" : ''}} onClick={()=>handleActive(4)}>
-              <Dropdown.Toggle className='nav-item text-light border-0' variant="" id="nav-dropdown">
-              محصولات
+            <Dropdown
+              className="rounded"
+              style={{ backgroundColor: active == 4 ? "rgb(37, 0, 122)" : "" }}
+              onClick={() => handleActive(4)}
+            >
+              <Dropdown.Toggle
+                className="nav-item text-light border-0"
+                variant=""
+                id="nav-dropdown"
+              >
+                محصولات
               </Dropdown.Toggle>
 
-              <Dropdown.Menu className='border p-0' id='nav-drop-down-menu'>
-              <Dropdown.Item className='text-end text-light rounded' onClick={()=>setSearchValue()} id='nav-drop-down-item' href="#/accessories">پیش فرض</Dropdown.Item>
-                <Dropdown.Item className='text-end text-light rounded' onClick={()=>setSearchValue("لپ تاپ")} id='nav-drop-down-item' href="#/laptop">لپ تاپ</Dropdown.Item>
-                <Dropdown.Item className='text-end text-light rounded' onClick={()=>setSearchValue("موبایل")} id='nav-drop-down-item' href="#/mobile">موبایل</Dropdown.Item>
-                <Dropdown.Item className='text-end text-light rounded' onClick={() => setSearchValue("لوازم جانبی")} id='nav-drop-down-item' href="#/accessories">لوازم جانبی</Dropdown.Item>
-                </Dropdown.Menu>
+              <Dropdown.Menu className="border p-0" id="nav-drop-down-menu">
+                <Dropdown.Item
+                  className="text-end text-light rounded"
+                  onClick={() => setSearchValue()}
+                  id="nav-drop-down-item"
+                  href="#/accessories"
+                >
+                  پیش فرض
+                </Dropdown.Item>
+                <Dropdown.Item
+                  className="text-end text-light rounded"
+                  onClick={() => setSearchValue("لپ تاپ")}
+                  id="nav-drop-down-item"
+                  href="#/laptop"
+                >
+                  لپ تاپ
+                </Dropdown.Item>
+                <Dropdown.Item
+                  className="text-end text-light rounded"
+                  onClick={() => setSearchValue("موبایل")}
+                  id="nav-drop-down-item"
+                  href="#/mobile"
+                >
+                  موبایل
+                </Dropdown.Item>
+                <Dropdown.Item
+                  className="text-end text-light rounded"
+                  onClick={() => setSearchValue("لوازم جانبی")}
+                  id="nav-drop-down-item"
+                  href="#/accessories"
+                >
+                  لوازم جانبی
+                </Dropdown.Item>
+              </Dropdown.Menu>
             </Dropdown>
-
           </ul>
-
         </div>
 
         <div className="d-flex align-items-center col-lg-7 col-sm-12">
-
           <form action="" className="col-5 form-inline p-1">
-            <div className="input-group">           
+            <div className="input-group">
               <div className="input-group-append pt-2 px-1">
-                <i className="" style={{ fontSize: '15px', cursor: 'pointer' }}>
-                  <img src={search} className="mb-2" style={{ cursor: 'pointer', width: '20px', height: '20px' }} />
+                <i className="" style={{ fontSize: "15px", cursor: "pointer" }}>
+                  <img
+                    src={search}
+                    className="mb-2"
+                    style={{ cursor: "pointer", width: "20px", height: "20px" }}
+                  />
                 </i>
               </div>
               <input
                 value={navSearchInputValue}
-                onChange={(e)=>setNavSearchInputValue(e.target.value)}
+                onChange={(e) => setNavSearchInputValue(e.target.value)}
                 type="text"
                 placeholder="جست و جو"
                 className="text-white form-control rounded border-0 search-input"
-                id='nav-search-input'
+                id="nav-search-input"
                 style={{
                   boxShadow: "none",
                 }}
               />
-
-
             </div>
           </form>
 
@@ -332,39 +417,72 @@ function Navbar() {
               </i>
             </div>
             <div className="p-1 ">
-            <MyDatePicker ></MyDatePicker>
+              <MyDatePicker></MyDatePicker>
             </div>
           </div>
 
           <div className="m- col-4 my-3">
             <div className="">
-              <Link to="./login" onClick={handleLogin} className="" style={{ display: token ? 'none' : 'block', textDecoration: 'none' }}>
-                <i className="text-white d-flex justify-content-end m-1" style={{ fontSize: '15px', cursor: 'pointer' }}>
+              <Link
+                to="./login"
+                onClick={handleLogin}
+                className=""
+                style={{
+                  display: token ? "none" : "block",
+                  textDecoration: "none",
+                }}
+              >
+                <i
+                  className="text-white d-flex justify-content-end m-1"
+                  style={{ fontSize: "15px", cursor: "pointer" }}
+                >
                   <div className="p-2 rounded lilo">
-                    <img src={userIcon} className="col-3" style={{ cursor: 'pointer', width: '20px', height: '20px' }} />
+                    <img
+                      src={userIcon}
+                      className="col-3"
+                      style={{
+                        cursor: "pointer",
+                        width: "20px",
+                        height: "20px",
+                      }}
+                    />
                     ثبت نام/ورود
                   </div>
                 </i>
-
               </Link>
             </div>
 
-            <div className="text-center" >
-              <Link to="./" onClick={handleExit} className="" style={{ display: token ? 'block' : 'none', textDecoration: 'none'}}>
-                <i className="text-white d-flex justify-content-end" style={{ fontSize: '15px', cursor: 'pointer'}}>
+            <div className="text-center">
+              <Link
+                to="./"
+                onClick={handleExit}
+                className=""
+                style={{
+                  display: token ? "block" : "none",
+                  textDecoration: "none",
+                }}
+              >
+                <i
+                  className="text-white d-flex justify-content-end"
+                  style={{ fontSize: "15px", cursor: "pointer" }}
+                >
                   <div className="p-2 rounded lilo">
-                    <img src={exit} className="col-3 mx-1 mt-" style={{ cursor: 'pointer', width: '18px', height: '18px'}} />
+                    <img
+                      src={exit}
+                      className="col-3 mx-1 mt-"
+                      style={{
+                        cursor: "pointer",
+                        width: "18px",
+                        height: "18px",
+                      }}
+                    />
                     خروج
                   </div>
                 </i>
-
               </Link>
+            </div>
           </div>
-
-          </div>
-        
         </div>
-
       </div>
     </nav>
   );
